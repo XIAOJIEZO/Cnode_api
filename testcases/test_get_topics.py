3b6ccf2995055fa96eeb944e84c070003573aaa3
2601804605@qq.com <Liujie26018>
2021-05-05 14:52:22 +0800

    优化

--- testcases/test_get_topics.py
+++ testcases/test_get_topics.py
@@ -1,10 +1,14 @@
 import pytest
+import allure
 from tools import methods
 from tools.file_untils import parse_json_file as Json
 
 get_topics = Json('topics.json', 'get_topics')
 
 
+@allure.feature('主题首页')
+@allure.story('非异常场景')
+@allure.title("{description}")
 @pytest.mark.parametrize("payload, description", get_topics)
 def test_get_topics(payload, description):
     r = methods.Methods().get(r'/api/v1/topics', payload)
@@ -14,6 +18,9 @@ def test_get_topics(payload, description):
 get_topics_abnormal = Json('topics.json', 'get_topics_abnormal')
 
 
+@allure.feature('主题首页')
+@allure.story('异常场景')
+@allure.title("{description}")
 @pytest.mark.parametrize("payload, response, description", get_topics_abnormal)
 def test_topics_abnormal(payload, response, description):
     r = methods.Methods().get(r'/api/v1/topics', payload)
